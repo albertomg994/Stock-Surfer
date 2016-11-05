67d5b3be811190aed215a0390bf64ad2fbe415de
ismaro3 <ismaro.394@gmail.com>
2016-11-05 02:36:45 +0100

    ¨Cosas¨

--- server.py
+++ server.py
@@ -2,6 +2,6 @@ import SimpleHTTPServer
 import SocketServer
 PORT = 8000
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
+httpd = SocketServer.TCPServer(("0.0.0.0", PORT), Handler)
 print "serving at port", PORT
 httpd.serve_forever()
